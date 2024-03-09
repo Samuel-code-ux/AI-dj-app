3a2247078c2018b83ac8973fbe3c23d810d8d384
Samuel-code-ux <79413869+Samuel-code-ux@users.noreply.github.com>
2024-03-09 22:00:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+song = "";
 song1 ="";
 song2 = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 righttWristY = 0;
+scoreLeftWrist = 0;
 function preload(){
 song1 = loadSound("music.mp3");
 song2 = loadSound("music2.mp3");
@@ -22,6 +24,8 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist =results[0].pose.keypoints[9].score;
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX + "leftWristY =" +leftWristY);
@@ -39,4 +43,27 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("red");
+    stroke("red");
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimal = floor(InNumberleftWristY);
+        volume = remove_decimal/500;
+        document.getElementById("volume").innerHTML = "Volume =" + volume;
+        song.setVolume(volume);
+        song2.stop();
+    }
+    if(song1 = false)
+    {
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimal = floor(InNumberleftWristY);
+        volume = remove_decimal/500;
+        document.getElementById("volume").innerHTML = "Volume =" + volume;
+        song.setVolume(volume);
+        song1.play();
+    }
 }
